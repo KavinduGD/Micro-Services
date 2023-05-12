452b20328d710b406d72b2d2100d770e69dab491
it21038914 <it21038914@my.sliit.lk>
2023-05-12 13:44:11 +0530

    handling

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -20,7 +20,7 @@ app.post("/posts/:id/comments", async (req, res) => {
 
   const comments = commentsByPostId[req.params.id] || [];
 
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
 
   commentsByPostId[req.params.id] = comments;
 
@@ -30,6 +30,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
