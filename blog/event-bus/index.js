c98a1db5e5ade1a1d1289bc7d7594bdda9b33356
it21038914 <it21038914@my.sliit.lk>
2023-05-18 00:19:24 +0530

    event sync

--- blog/event-bus/index.js
+++ blog/event-bus/index.js
@@ -5,8 +5,13 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  event.push(event);
+
   // console.log(event);
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
@@ -16,6 +21,9 @@ app.post("/events", (req, res) => {
   res.send({ status: "Ok" });
 });
 
+app.get("/events", (req, res) => {
+  res.send(events);
+});
 app.listen(4005, () => {
   console.log("listening on 4005 ");
 });
--- blog/query/index.js
+++ blog/query/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
+const axios = require("axios");
 const app = express();
 
 app.use(bodyParser.json());
@@ -9,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get("/posts", (req, res) => {
-  res.send(posts);
-});
-
-app.post("/events", (req, res) => {
-  const { type, data } = req.body;
-
+const handleEvents = (type, data) => {
   if (type === "PostCreated") {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -39,11 +33,26 @@ app.post("/events", (req, res) => {
     comment.status = status;
     comment.content = content;
   }
+};
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
 
+app.post("/events", (req, res) => {
+  const { type, data } = req.body;
+
+  handleEvents(type, data);
   console.log(posts);
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log("listening on 4002");
+  const res = await axios.get("http://localhost:4005/events");
+
+  for (let event of res.data) {
+    console.log("Processing event :", event.type);
+
+    handleEvents(event.type, event.data);
+  }
 });
